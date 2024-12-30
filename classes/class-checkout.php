caf2df3170bc07dd2bc8bcbe375483b14f0bf84b
Nevma.gr <gkastorinis@gmail.com>
2024-12-30 08:03:53 +0200

    Refactor donation form and enhance ACF integration in nvm-woo-donor.php
    - Updated the donation form to ensure proper initialization and display of the custom amount field.
    - Added a new ACF field for product selection to improve donation options.
    - Cleaned up the ACF JSON configuration by removing deprecated settings.
    - Commented out unused action hooks for better code clarity and maintenance.
    - Improved user experience by validating and displaying the custom donation input correctly.

---
+++ classes/class-checkout.php
@@ -0,0 +1,20 @@
+<?php //phpcs:ignore - \r\n issue
+
+/**
+ * Set namespace.
+ */
+namespace Nvm\Donor;
+
+/**
+ * Check that the file is not accessed directly.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'We\'re sorry, but you can not directly access this file.' );
+}
+
+/**
+ * Class Admin_Menu.
+ */
+class Nvm_Checkout {
+
+}
