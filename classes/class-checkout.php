83c3609a51c0a94e2333d59b9a926231467bfe1a
Nevma.gr <gkastorinis@gmail.com>
2024-12-31 12:13:19 +0200

    Enhance checkout process and donation fields in class-checkout.php
    - Added new action hooks to display and process custom fields for 'timologio' orders during checkout.
    - Updated JavaScript to dynamically show/hide donation-specific fields based on user selection.
    - Refactored billing fields to include new fields for VAT ID, company name, and activity, improving the donation checkout experience.
    - Implemented validation for required fields in 'timologio' orders to ensure necessary information is collected.
    - Improved organization of action hooks and methods for better maintainability.

--- classes/class-checkout.php
+++ classes/class-checkout.php
@@ -22,6 +22,8 @@ class Checkout {
 	public function __construct() {
 
 		add_action( 'template_redirect', array( $this, 'initiate_checkout_actions' ) );
+		add_action( 'woocommerce_admin_order_data_after_billing_address', array( $this, 'show_timologio_fields' ), 10, 1 );
+		add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'timologio_checkout_field_update_order_meta' ) );
 	}
 
 	/**
@@ -34,10 +36,15 @@ class Checkout {
 
 			add_action( 'woocommerce_before_checkout_billing_form', array( $this, 'add_donors_ways' ) );
 
-			add_filter( 'woocommerce_checkout_fields', array( $this, 'customize_checkout_fields' ), 10 );
+			add_filter( 'woocommerce_checkout_fields', array( $this, 'customize_checkout_fields' ), 20 );
+
+			add_filter( 'woocommerce_checkout_fields', array( $this, 'custom_woocommerce_billing_fields' ), 30 );
 
 			// remove Coupon if had donor product
 			add_filter( 'woocommerce_coupons_enabled', array( $this, 'remove_coupon_code_field_cart' ), 10 );
+
+			add_action( 'woocommerce_checkout_process', array( $this, 'timologio_process' ) );
+
 		}
 	}
 
@@ -46,7 +53,6 @@ class Checkout {
 	}
 
 	public function add_donors_ways() {
-
 		$chosen = WC()->session->get( 'donation' );
 		$chosen = empty( $chosen ) ? WC()->checkout->get_value( 'donation' ) : $chosen;
 
@@ -77,36 +83,147 @@ class Checkout {
 			}
 		</style>
 
-	<script type="text/javascript">
-		document.addEventListener('DOMContentLoaded', function () {
-			const orderTypeRadios = document.querySelectorAll('input[name="type_of_donation"]');
+		<script type="text/javascript">
+			document.addEventListener('DOMContentLoaded', function () {
+				const donationRadios = document.querySelectorAll('input[name="type_of_donation"]');
 
-			function updateDisplay() {
-				const selectedValue = document.querySelector('input[name="type_of_donation"]:checked').value;
-				const displayStyle = selectedValue === 'timologio' ? 'block' : 'none';
-				document.querySelectorAll('.timologio').forEach(el => el.style.display = displayStyle);
-			}
+				function updateDisplay() {
+					// Get the selected donation type
+					const selectedValue = document.querySelector('input[name="type_of_donation"]:checked').value;
 
-			// Check initially before any clicks
-			updateDisplay();
+					// Hide all donation-specific elements
+					document.querySelectorAll('.donation-section').forEach(el => el.style.display = 'none');
 
-			// Update display on radio button change
-			orderTypeRadios.forEach(radio => radio.addEventListener('click', updateDisplay));
-		});
-	</script>
+					// Show all elements that match the selected type
+					document.querySelectorAll(`.donation-${selectedValue}`).forEach(el => el.style.display = 'block');
+				}
+
+				// Check initially before any clicks
+				updateDisplay();
+
+				// Update display on radio button change
+				donationRadios.forEach(radio => radio.addEventListener('click', updateDisplay));
+			});
+		</script>
 		<?php
 	}
 
+
 	public function customize_checkout_fields( $fields ) {
 
-			unset( $fields['billing']['billing_company'] );
-			unset( $fields['billing']['billing_address_1'] );
-			unset( $fields['billing']['billing_address_2'] );
-			unset( $fields['billing']['billing_postcode'] );
-			unset( $fields['billing']['billing_state'] );
+		$fields['billing']['billing_email']['priority'] = 35;
+		// $fields['billing']['billing_phone']['priority'] = 38;
+
+		$fields['billing']['billing_country']['priority']  = 75;
+		$fields['billing']['billing_postcode']['priority'] = 66;
+
+		$fields['billing']['billing_country']['class'][0]  = 'form-row-first';
+		$fields['billing']['billing_postcode']['class'][0] = 'form-row-last';
+
+		unset( $fields['billing']['billing_company'] );
+		unset( $fields['billing']['billing_address_2'] );
+		unset( $fields['billing']['billing_state'] );
 
 		return $fields;
 	}
+
+
+	/**
+	 * Customizes WooCommerce billing fields.
+	 *
+	 * @param array $fields The existing billing fields.
+	 * @return array Modified billing fields.
+	 */
+	public function custom_woocommerce_billing_fields( $fields ) {
+
+		// Define new fields.
+		$new_fields = array(
+			'billing_tax_office'  => array(
+				'label'    => __( 'ΔΟΥ *', 'nevma' ),
+				'required' => false,
+				'clear'    => false,
+				'type'     => 'text',
+				'class'    => array( 'donation-section', 'form-row-wide', 'donation-corporate', 'donation-memoriam' ),
+			),
+			'billing_company_nvm' => array(
+				'label'    => __( 'Επωνυμία Εταιρίας *', 'nevma' ),
+				'required' => false,
+				'clear'    => false,
+				'type'     => 'text',
+				'class'    => array( 'donation-section', 'form-row-wide', 'donation-corporate', 'donation-memoriam' ),
+			),
+			'billing_activity'    => array(
+				'label'    => __( 'Δραστηριότητα *', 'nevma' ),
+				'required' => false,
+				'clear'    => false,
+				'type'     => 'text',
+				'class'    => array( 'donation-section', 'form-row-last', 'donation-corporate', 'donation-memoriam' ),
+			),
+			'billing_vat_id'      => array(
+				'label'    => __( 'ΑΦΜ *', 'nevma' ),
+				'required' => false,
+				'clear'    => false,
+				'type'     => 'text',
+				'class'    => array( 'donation-section', 'form-row-first', 'donation-corporate', 'donation-memoriam' ),
+			),
+			'billing_person_gone' => array(
+				'label'    => __( 'ΑΦΜ *', 'nevma' ),
+				'required' => false,
+				'clear'    => false,
+				'type'     => 'text',
+				'class'    => array( 'donation-section', 'form-row-first', 'donation-corporate', 'donation-memoriam' ),
+			),
+		);
+
+		// Merge new fields with existing billing fields.
+		$fields['billing'] = array_merge( $new_fields, $fields['billing'] );
+
+		return $fields;
+	}
+
+
+
+	/**
+	 *
+	 * This file contains the 'timologio_process' function, which is hooked to the 'woocommerce_checkout_process' action.
+	 * The function is responsible for validating and processing orders with the 'timologio' type.
+	 * It checks if the required fields for 'timologio' orders are filled in and displays error notices if any field is missing.
+	 * It also checks if the shipping method is 'wc_pickup_store' and displays an error notice if the pickup store is not selected.
+	 *
+	 * @since 1.0.0
+	 */
+	public function timologio_process() {
+		global $woocommerce;
+
+		if ( $_POST['type_of_order'] == 'timologio' ) {
+			if ( $_POST['billing_vat_id'] == '' ) {
+				wc_add_notice( __( 'Συμπληρώστε το πεδίο ΑΦΜ' ), 'error' );
+			}
+			if ( $_POST['billing_activity'] == '' ) {
+				wc_add_notice( __( 'Συμπληρώστε την Δραστηριότητα' ), 'error' );
+			}
+			if ( $_POST['billing_company-nvm'] == '' ) {
+				wc_add_notice( __( 'Συμπληρώστε το πεδίο Επωνυμία εταιρίας' ), 'error' );
+			}
+		}
+	}
+
+
+
+	public function timologio_checkout_field_update_order_meta( $order_id ) {
+
+		update_post_meta( $order_id, '_billing_company', esc_attr( $_POST['billing_company-nvm'] ) );
+		update_post_meta( $order_id, '_type_of_order', esc_attr( $_POST['type_of_order'] ) );
+	}
+
+	public function show_timologio_fields( $order ) {
+		if ( get_post_meta( $order->id, '_billing_vat_id', true ) != '' ) {
+			echo '<p><strong>AFM:</strong> ' . get_post_meta( $order->id, '_billing_vat_id', true ) . '</p>';
+			echo '<p><strong>Δραστηριότητα:</strong> ' . get_post_meta( $order->id, '_billing_activity', true ) . '</p>';
+			echo '<p><strong>Επωνυμία Εταιρίας:</strong> ' . get_post_meta( $order->id, '_billing_company', true ) . '</p>';
+		}
+	}
+
 	public function initiate_redirect_template() {
 
 		$nvm_product = new Nvm_Product();
